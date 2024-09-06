1ddbe4b7f1aa6cc5443228f93db74328476bb15c
Kuzry <krzysztof.kzr1@gmail.com>
2024-09-06 08:00:26 +0200

    fix: small

--- apps/workflows/src/forms/Auth/ResetPassword/action.ts
+++ apps/workflows/src/forms/Auth/ResetPassword/action.ts
@@ -4,7 +4,6 @@ import { createServerAction } from "zsa";
 import { getTranslations } from "next-intl/server";
 import { getResetPasswordFormSchema } from "@/forms/Auth/ResetPassword/schema";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
-import { getUser } from "@/utils/auth/helpers";
 
 export const resetPasswordFormAction = createServerAction()
   .input(async () =>
